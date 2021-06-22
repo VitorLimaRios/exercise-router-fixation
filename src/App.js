04647a08f415596f06d420c01b75e1392fc6a447
VitorLimaRios <vitorlimarios999@gmail.com>
2021-06-22 13:47:55 -0300

    adicionando reqs 1 ao 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/" component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/users" component={ Users } />
+      </BrowserRouter>
     );
   }
 }
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Users = () => (
+const Users = ({ greetingMessage = 'Hi There' }) => (
   <div>
     <h2> Users </h2>
-    <p> My awesome Users component </p>
+    <p> {greetingMessage}, this is my awesome Users component </p>
   </div>
 );
 
 export default Users;
+
